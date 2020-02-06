41d1f3c8898066942f3dc42037c4ff17e4796ff2
JoaoCostaIFG <joaocosta.tra@outlook.com>
2020-02-06 21:22:08 +0000

    No idea with I'm doing but gonna use the hello world skill as base.

--- __init__.py
+++ __init__.py
@@ -6,30 +6,42 @@ import youtube_dl
 
 class YoutubedlSkill(MycroftSkill):
     def __init__(self):
+        """ The __init__ method is called when the Skill is first constructed.
+        It is often used to declare variables or perform setup actions, however
+        it cannot utilise MycroftSkill methods as the class does not yet exist.
+        """
         super().__init__()
-        self.playing = False
+        self.playing = True
 
     def initialize(self):
         """ Perform any final setup needed for the skill here.
         This function is invoked after the skill is fully constructed and
         registered with the system. Intents will be registered and Skill
         settings will be available."""
-        pass
+        my_setting = self.settings.get("my_setting")
 
-    @intent_handler(IntentBuilder("youtubedl.intent").require("YoutubedlKeyword.voc"))
-    def handle_playintent(self, message):
-        vid_name = message.data.get("vid")
-        if vid_name is not None:
-            self.speak_dialog(vid_name)
-        else:
-            self.speak_dialog("dunno vid name")
+    @intent_handler(IntentBuilder("YoutubedlIntent").require("YoutubedlKeyword"))
+    def handle_thank_you_intent(self, message):
+        """ This is an Adapt intent handler, it is triggered by a keyword."""
+        self.speak_dialog("welcome")
+
+    #  @intent_handler("HowAreYou.intent")
+    #  def handle_how_are_you_intent(self, message):
+        #  """ This is a Padatious intent handler.
+        #  It is triggered using a list of sample phrases."""
+        #  self.speak_dialog("how.are.you")
+
+    #  @intent_handler(IntentBuilder("HelloWorldIntent").require("HelloWorldKeyword"))
+    #  def handle_hello_world_intent(self, message):
+        #  """ Skills can log useful information. These will appear in the CLI and
+        #  the skills.log file."""
+        #  self.log.info(
+            #  "There are five types of log messages: "
+            #  "info, debug, warning, error, and exception."
+        #  )
+        #  self.speak_dialog("hello.world")
 
     def stop(self):
-        # what to do if told to stop
-        pass
-
-    def shutdown(self):
-        self.stop(self)
         pass
 
 
